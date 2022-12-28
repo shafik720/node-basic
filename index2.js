9f5b33520af324f05a0a6b3dfbf986f6bc2e95fc
shafik720 <shafikrasel5@gmail.com>
2022-12-28 14:12:28 +0600

    practicing node basic

--- index2.js
+++ index2.js
@@ -7,6 +7,17 @@ app.get('/',(req,res)=>{
     res.send('another try');
 })
 
+const user = [
+    { name : 'rasel', id: 1 , profession : 'web developer'},
+    { name : 'abir', id: 2 , profession : 'banker '},
+    { name : 'shafik', id: 3 , profession : 'mariner'},
+    { name : 'surjo', id: 4 , profession : 'job holder'}
+]
+
+app.get('/users',(req,res)=>{
+    res.send(user);
+})
+
 app.listen(port,()=>{
     console.log('going in 2500');
 })
