e202d6d5cfba44c5f30b14089371363dcb7588f7
shafik720 <shafikrasel5@gmail.com>
2022-12-28 14:43:17 +0600

    practicing node basic

--- index3.js
+++ index3.js
@@ -3,10 +3,24 @@ const express = require('express');
 const app = express();
 const port = 2100;
 
+const user = [
+    { name : 'rasel', id: 1 , profession : 'web developer'},
+    { name : 'abir', id: 2 , profession : 'banker '},
+    { name : 'shafik', id: 3 , profession : 'mariner'},
+    { name : 'surjo', id: 4 , profession : 'job holder'}
+]
+
 app.get('/',(req, res)=>{
     res.send('ok go on');
 })
-
+app.get('/users',(req,res)=>{
+    res.send(user);
+})
+app.get('/users/:id',(req, res)=>{
+    const id = req.params.id;
+    const users =  user.find(index=>index.id === parseInt(id));
+    res.send(users);
+})
 app.listen(port, ()=>{
     console.log('going for 2100');
 });
