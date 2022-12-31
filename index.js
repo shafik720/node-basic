33e00679a466a5576065d55278dc5d99ec77a93e
shafik720 <shafikrasel5@gmail.com>
2022-12-31 20:05:23 +0600

    trying mongodb firsttime

--- index.js
+++ index.js
@@ -18,13 +18,21 @@ app.get('/',(req, res)=>{
 
 const uri = "mongodb+srv://rasel:Bwv139VgeyUuFiAC@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('connected successfully !');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db('foodexpress').collection('all food');
+        const user = {name: 'Abir Rahman', email :'abir@gmail.com'};
+        const result = await userCollection.insertOne(user);
 
+        console.log(`User is installed with id : ${result.insertedId} `)
+    }
+    finally{
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 
 app.listen(port, ()=>{
