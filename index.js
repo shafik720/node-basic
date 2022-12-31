9bd6d6757603da00d5cc4ab9f8eaf0307f1349a2
shafik720 <shafikrasel5@gmail.com>
2022-12-31 21:40:28 +0600

    trying mongodb firsttime

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 5000;
@@ -9,27 +10,31 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.send('another 2nd try');
-})
+
 
 // user : rasel
-// pass : Bwv139VgeyUuFiAC
+// pass : 15kMpBXxqfu3liKz
 
-const uri = "mongodb+srv://rasel:Bwv139VgeyUuFiAC@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://rasel:15kMpBXxqfu3liKz@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         await client.connect();
         const userCollection = client.db('foodexpress').collection('all food');
-        // const user = {name: 'Abir Rahman', email :'abir@gmail.com'};
+        // const user = {name: 'ad sen', email :'tusar@tusar.com'};
         // const result = await userCollection.insertOne(user);
-        // console.log(`User is installed with id : ${result.insertedId} `)
         
+        // app.post('/user', async(req, res)=>{
+        //     const newUser = req.body;
+        //     console.log('requesting new user ', newUser);
+        //     const result = await userCollection.insertOne(newUser);
+        //     res.send(result);
+        //     console.log(`User is installed with id : ${result.insertedId} `)
+        // });
         app.post('/user', (req, res)=>{
             const newUser = req.body;
-            console.log('requesting new user ', newUser);
-            res.send(newUser);
+            console.log('requesting new user', newUser);
+            res.send({result : 'success'})
         })
     }
     finally{
