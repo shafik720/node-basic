ca3aadd1a1670417478c578885450ba73295759d
shafik720 <shafikrasel5@gmail.com>
2023-01-05 19:07:02 +0600

    trying node second time

--- 2nd Try/index.js
+++ 2nd Try/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/user',(req, res)=>{
-    res.send('trying for second user');
+app.post('/user',(req, res)=>{
+    const user = req.body;
+    res.send(user);
 })
+
+app.get('/', (req, res)=>{
+    res.send('hellow');
+})
+
 
 app.listen(port,()=>{
     console.log('listening to port 5000');
