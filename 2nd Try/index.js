40605e19f9e270a5181e88f5a0f2296f344bc59f
shafik720 <shafikrasel5@gmail.com>
2023-01-05 19:36:11 +0600

    trying node second time

--- 2nd Try/index.js
+++ 2nd Try/index.js
@@ -6,14 +6,41 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/user',(req, res)=>{
-    const user = req.body;
-    res.send(user);
-})
+// using mongodb here
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rasel:QJcHAVAcEsRn6ymq@cluster0.0ihcm8w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res)=>{
     res.send('hellow');
 })
+
+
+
+async function run() {
+    try {      
+      const userCollection = client.db('2ndTime').collection('foods');
+
+      // adding a user
+      app.post('/user',async(req, res)=>{
+        const user = req.body;        
+        const result = await userCollection.insertOne(user); 
+        res.send(result);
+    
+    })
+    } finally {
+    //   await client.close();
+    }
+  }
+
+  run().catch(console.dir);
+
+
 
 
 app.listen(port,()=>{
